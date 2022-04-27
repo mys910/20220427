c9820e4c367be5ee0d382d761255d29f2677b1a4
SungYongMo <mys910@gmail.com>
2022-04-27 17:50:31 +0900

    123

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # 20220427
 # HelloWorld 만들기
 ## 처음 만든 프로젝트
-### 123
-#### dldldldl
-###dfjiofjewoifhj
--- main.cpp
+++ main.cpp
@@ -4,8 +4,28 @@ using namespace std;
 
 int main()
 {
-	cout << "HelloWorld" << endl;
-	cout << "헬로우 월드" << endl;
+	int a = 10;
+
+	cout << a + 2 << endl;
+	cout << a + 2 << endl;
+	cout << a + 2 << endl;
+	cout << a + 2 << endl;
+	cout << a % 3 << endl;
+	
+	float b = 1.9f;
+
+	cout << b + 2 << endl;
+	cout << b + 2 << endl;
+	cout << b + 2 << endl;
+	cout << b + 2 << endl;
+
+	char r = 'R'; // R이 아스키코드로 변환되어 연산됨
+
+	cout << (char)(r + 2) << endl;
+	cout << (char)(r - 2) << endl;
+	cout << (char)(r * 2) << endl;
+	cout << (char)(r / 2) << endl;
+
 
 	return 0;
 }
